4980af27cf0da295f5cb330b0a31da28d81914cb
Ryan Xavier <lugialga@gmail.com>
2023-12-06 10:57:13 -0300

    Criação de menu responsivo
    Nesse commit, foi alterado o menu antigo, para um menu responsivo

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+function menuShow(){
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')){
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "img/menu.svg";
+    } else {
+        menuMobile.classList.add('open')
+        document.querySelector('.icon').src = "img/close.svg";
+    } 
+}
+
+
 let slides = document.querySelectorAll('.slide-container');
 let index = 0;
 
